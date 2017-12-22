ae77bd93c20166ed6ca55d75b914a18720e2b96c
wladimir <chief.kraftvoll@yandex.ru>
2017-12-22 22:05:26 +0300

    Add Line

--- src/main/java/game/Main.java
+++ src/main/java/game/Main.java
@@ -6,6 +6,8 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("ich arbeite ersten mal an GitHub!");
+        System.out.println();
         Model model = new Model();
         Controller controller = new Controller(model);
         JFrame game = new JFrame();
